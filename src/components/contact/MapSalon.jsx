e2048b0fe7fc95f2a897b9999f29fad8fff59f11
Dilwar96 <157256888+Dilwar96@users.noreply.github.com>
2024-07-21 01:01:10 +0200

    change the map attribute

--- src/components/contact/MapSalon.jsx
+++ src/components/contact/MapSalon.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css"; // Ensure Leaflet CSS is imported
 
 import marker from "../../Assets/icons/Location.svg";
 import { Icon } from "leaflet";
