3ec7acc83a4717e107b75c2d16288d3cc8814cf5
Dilwar96 <157256888+Dilwar96@users.noreply.github.com>
2024-07-21 00:56:38 +0200

    change the map attribute

--- src/components/contact/MapSalon.jsx
+++ src/components/contact/MapSalon.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css"; // Ensure Leaflet CSS is imported
 
+import marker from "../../Assets/icons/Location.svg";
+import { Icon } from "leaflet";
+const myIcon = new Icon({
+  iconUrl: marker,
+  iconSize: [32, 32],
+});
+
 const MapSalon = () => {
   const position = [53.908313, 9.483062]; // Update with your actual salon coordinates
 
@@ -13,10 +20,10 @@ const MapSalon = () => {
       className="w-full h-full" // Ensure the map container fills the available space
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
+      <Marker position={position} icon={myIcon}>
         <Popup>
           Mardin FriseurSalon <br /> lübscherkamp 2-6, 25524 Itzehoe
         </Popup>
